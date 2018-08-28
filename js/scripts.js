96629940449935434d9672aadaa7b0f8665b6a54
derrick12 <derrickwaweru@gmail.com>
2018-08-28 17:02:22 +0300

    add edits to function

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,39 @@ function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function Player(name, score, turn) {
+    this.name = name;
+    this.score = score;
+    this.turn = turn;
+}
+
+
+//Front end logic
+$("#letsPlay").click(function() {
+    player1.name = $("input#player1").val();
+    player2.name = $("input#player2").val();
+
+    $("#name1").text(player1.name);
+    $("#name2").text(player2.name);
+    $("#displayScore1").text(player1.score);
+    $("#displayScore2").text(player2.score);
+    $("#displayTotal").text(total);
+
+    $("#diceGame").show();
+});
+
+
+
+
+/*
 function Player(first, second) {
     this.firstPlayer = first;
     this.secondPlayer = second;
 }
+*/
 
+/*
 
-
-
-//Front end logic
 $(document).ready(function() {
     $("form#player-names").submit(function(event) {
         event.preventDefault();
@@ -19,20 +43,16 @@ $(document).ready(function() {
         var inputtedFirstPlayer = $("input#player1").val();
         var inputtedSecondPlayer = $("input#player2").val();
 
+
         var newPlayers = new Player(inputtedFirstPlayer, inputtedSecondPlayer);
 
-        /*
-        $("#player1Name").text(player1);
-        $("#player2Name").text(player2);
-        */
-
+        
         $("h2#name1").append("<span class='Player'>" + newPlayers.firstPlayer + "</span>");
         $("h2#name2").append("<span class='Player'>" + newPlayers.secondPlayer + "</span>");
 
         $("input#player1").val("");
         $("input#player2").val("");
 
-
-
     });
 });
+*/
