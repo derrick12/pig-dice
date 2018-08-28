ea644db1e56e91642006acfdbc2c82ca1149b80e
derrick12 <derrickwaweru@gmail.com>
2018-08-28 17:19:34 +0300

    add prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -9,50 +9,68 @@ function Player(name, score, turn) {
     this.turn = turn;
 }
 
+Player.prototype.startTurn = function() {
+    this.turn = 1;
+};
+
+Player.prototype.endTurn = function() {
+    this.turn = 0;
+};
+
+Player.prototype.hold = function(total) {
+    this.score += total;
+};
+
+
+
 
 //Front end logic
-$("#letsPlay").click(function() {
-    player1.name = $("input#player1").val();
-    player2.name = $("input#player2").val();
+$(function() {
 
-    $("#name1").text(player1.name);
-    $("#name2").text(player2.name);
-    $("#displayScore1").text(player1.score);
-    $("#displayScore2").text(player2.score);
-    $("#displayTotal").text(total);
+            var total = 0;
 
-    $("#diceGame").show();
-});
+            $("#letsPlay").click(function() {
+                player1.name = $("input#player1").val();
+                player2.name = $("input#player2").val();
 
+                $("#name1").text(player1.name);
+                $("#name2").text(player2.name);
+                $("#displayScore1").text(player1.score);
+                $("#displayScore2").text(player2.score);
+                $("#displayTotal").text(total);
 
-
-
-/*
-function Player(first, second) {
-    this.firstPlayer = first;
-    this.secondPlayer = second;
-}
-*/
-
-/*
-
-$(document).ready(function() {
-    $("form#player-names").submit(function(event) {
-        event.preventDefault();
-
-        var inputtedFirstPlayer = $("input#player1").val();
-        var inputtedSecondPlayer = $("input#player2").val();
+                $("#diceGame").show();
+            });
 
 
-        var newPlayers = new Player(inputtedFirstPlayer, inputtedSecondPlayer);
 
-        
-        $("h2#name1").append("<span class='Player'>" + newPlayers.firstPlayer + "</span>");
-        $("h2#name2").append("<span class='Player'>" + newPlayers.secondPlayer + "</span>");
 
-        $("input#player1").val("");
-        $("input#player2").val("");
+            /*
+            function Player(first, second) {
+                this.firstPlayer = first;
+                this.secondPlayer = second;
+            }
+            */
 
-    });
-});
-*/
+            /*
+
+            $(document).ready(function() {
+                $("form#player-names").submit(function(event) {
+                    event.preventDefault();
+
+                    var inputtedFirstPlayer = $("input#player1").val();
+                    var inputtedSecondPlayer = $("input#player2").val();
+
+
+                    var newPlayers = new Player(inputtedFirstPlayer, inputtedSecondPlayer);
+
+                    
+                    $("h2#name1").append("<span class='Player'>" + newPlayers.firstPlayer + "</span>");
+                    $("h2#name2").append("<span class='Player'>" + newPlayers.secondPlayer + "</span>");
+
+                    $("input#player1").val("");
+                    $("input#player2").val("");
+
+                });
+            });
+            */
